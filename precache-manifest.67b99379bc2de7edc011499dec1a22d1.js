3accefa51acd1d1b118bfe6c7141ffabcf815e56
wjh3490 <47782613+wjh3490@users.noreply.github.com>
2021-02-27 04:06:01 +0000

    Deploying to test from master c0c74c4577721c6b84c56c210c29ad4bebf8475e

---
+++ precache-manifest.67b99379bc2de7edc011499dec1a22d1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5d53817abb99bdb9a9",
+    "url": "/music-vue/css/app.c6de70be.css"
+  },
+  {
+    "revision": "763e3b83045914ce184d",
+    "url": "/music-vue/css/chunk-vendors.056ed3f3.css"
+  },
+  {
+    "revision": "158c2102fc2642704ab9",
+    "url": "/music-vue/css/detail.f2e472b4.css"
+  },
+  {
+    "revision": "9b98a970237ac39d0f39",
+    "url": "/music-vue/css/rank.a6aaf1f3.css"
+  },
+  {
+    "revision": "fa59f60c88eeb941e7a2",
+    "url": "/music-vue/css/recommend.61a25668.css"
+  },
+  {
+    "revision": "09fbcfb3d3c4a38ac264",
+    "url": "/music-vue/css/search.58c1cfcb.css"
+  },
+  {
+    "revision": "e6fa934a1aadb5f992f4",
+    "url": "/music-vue/css/user.214d19fa.css"
+  },
+  {
+    "revision": "30f9ea308af25bee5c2409e3130b799d",
+    "url": "/music-vue/fonts/iconfont.30f9ea30.ttf"
+  },
+  {
+    "revision": "3cb7f15d2123610feabae7eb0d02eda0",
+    "url": "/music-vue/fonts/iconfont.3cb7f15d.eot"
+  },
+  {
+    "revision": "348d746baa49472ac5974ee2bd14ed3a",
+    "url": "/music-vue/img/1.348d746b.jpg"
+  },
+  {
+    "revision": "3087ae71d5eb586dbe26b0c565858955",
+    "url": "/music-vue/img/2.3087ae71.jpg"
+  },
+  {
+    "revision": "998bc3427c9a9033a75a275b1a72c5a8",
+    "url": "/music-vue/img/3.998bc342.jpg"
+  },
+  {
+    "revision": "11a24ce91a0035a7881de6cd9711520b",
+    "url": "/music-vue/img/4.11a24ce9.jpg"
+  },
+  {
+    "revision": "5cb4e1308d6afad785fa8522a3460dfc",
+    "url": "/music-vue/img/5.5cb4e130.jpg"
+  },
+  {
+    "revision": "794ac2cd2337c6c862c43e80778ee0f4",
+    "url": "/music-vue/img/6.794ac2cd.jpg"
+  },
+  {
+    "revision": "cca7741d697c5cd532989eaca522e41a",
+    "url": "/music-vue/img/default.cca7741d.jpg"
+  },
+  {
+    "revision": "99219387fe93a7cb8473b08cde38b980",
+    "url": "/music-vue/img/iconfont.99219387.svg"
+  },
+  {
+    "revision": "18bc0c844beb71cf2728e69ca3ed7cea",
+    "url": "/music-vue/index.html"
+  },
+  {
+    "revision": "ed5d53817abb99bdb9a9",
+    "url": "/music-vue/js/app.e11fdf90.js"
+  },
+  {
+    "revision": "763e3b83045914ce184d",
+    "url": "/music-vue/js/chunk-vendors.c5b630f2.js"
+  },
+  {
+    "revision": "158c2102fc2642704ab9",
+    "url": "/music-vue/js/detail.eda887e6.js"
+  },
+  {
+    "revision": "9b98a970237ac39d0f39",
+    "url": "/music-vue/js/rank.bf2c757e.js"
+  },
+  {
+    "revision": "fa59f60c88eeb941e7a2",
+    "url": "/music-vue/js/recommend.8e762790.js"
+  },
+  {
+    "revision": "09fbcfb3d3c4a38ac264",
+    "url": "/music-vue/js/search.ccf8392e.js"
+  },
+  {
+    "revision": "e6fa934a1aadb5f992f4",
+    "url": "/music-vue/js/user.4d936ef8.js"
+  },
+  {
+    "revision": "93043ec0ee9d39ae69da6656210f44b3",
+    "url": "/music-vue/mainifest.json"
+  },
+  {
+    "revision": "beb686ffdbec7cccdb7ff78bb1fbac33",
+    "url": "/music-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/music-vue/robots.txt"
+  }
+]);
