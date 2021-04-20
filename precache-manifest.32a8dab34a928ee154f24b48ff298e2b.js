82ee3be2642687281da15712535323b73868b191
wjh3490 <47782613+wjh3490@users.noreply.github.com>
2021-04-20 00:36:22 +0000

    Deploying to test from @ wjh3490/music-vue@38ca7b393323ffbd6fcd846b746a70b7024b584b 🚀

---
+++ precache-manifest.32a8dab34a928ee154f24b48ff298e2b.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b4338a7a10776d8190",
+    "url": "/music-vue/css/album.917b0a55.css"
+  },
+  {
+    "revision": "3aca5dd10cfba55cb74b",
+    "url": "/music-vue/css/app.c245673d.css"
+  },
+  {
+    "revision": "0864a4d6ee2fd51bf571",
+    "url": "/music-vue/css/chunk-vendors.add8ea24.css"
+  },
+  {
+    "revision": "55bda3ce8f5b871496b7",
+    "url": "/music-vue/css/mall-category.63ab6bbe.css"
+  },
+  {
+    "revision": "d333620fa006e17b33bb",
+    "url": "/music-vue/css/mall.70e13aa0.css"
+  },
+  {
+    "revision": "ca2f5864abc45c0cdcaf",
+    "url": "/music-vue/css/playlist-detail.917b0a55.css"
+  },
+  {
+    "revision": "b1c4214d39721d693084",
+    "url": "/music-vue/css/playlist.bb5e0cad.css"
+  },
+  {
+    "revision": "4483e560d118e56f748f",
+    "url": "/music-vue/css/rank.3cf3bbfb.css"
+  },
+  {
+    "revision": "b0958f7570585bb6831f",
+    "url": "/music-vue/css/recommend.005c850f.css"
+  },
+  {
+    "revision": "2bc386d717b4293f10e2",
+    "url": "/music-vue/css/search.1c2dc72f.css"
+  },
+  {
+    "revision": "46ee12f6cf6ab7a8ad35",
+    "url": "/music-vue/css/singer-detail.f33934ca.css"
+  },
+  {
+    "revision": "ffc36ec116d61431ad11",
+    "url": "/music-vue/css/songs.b33cbde1.css"
+  },
+  {
+    "revision": "178c223001efb16b9b6e",
+    "url": "/music-vue/css/user.214d19fa.css"
+  },
+  {
+    "revision": "30f9ea308af25bee5c2409e3130b799d",
+    "url": "/music-vue/fonts/iconfont.30f9ea30.ttf"
+  },
+  {
+    "revision": "3cb7f15d2123610feabae7eb0d02eda0",
+    "url": "/music-vue/fonts/iconfont.3cb7f15d.eot"
+  },
+  {
+    "revision": "cca7741d697c5cd532989eaca522e41a",
+    "url": "/music-vue/img/default.cca7741d.jpg"
+  },
+  {
+    "revision": "384a22c5f97bf09b7262d63e8195a39f",
+    "url": "/music-vue/img/digital-album.384a22c5.png"
+  },
+  {
+    "revision": "99219387fe93a7cb8473b08cde38b980",
+    "url": "/music-vue/img/iconfont.99219387.svg"
+  },
+  {
+    "revision": "5e34b1b37bf9932e5149cfa90d3ff10e",
+    "url": "/music-vue/img/type_cd.5e34b1b3.png"
+  },
+  {
+    "revision": "974b6378c142b8987154bd2ee547f7f8",
+    "url": "/music-vue/index.html"
+  },
+  {
+    "revision": "a2b4338a7a10776d8190",
+    "url": "/music-vue/js/album.f0c63cb7.js"
+  },
+  {
+    "revision": "3aca5dd10cfba55cb74b",
+    "url": "/music-vue/js/app.6eab457d.js"
+  },
+  {
+    "revision": "0864a4d6ee2fd51bf571",
+    "url": "/music-vue/js/chunk-vendors.0f004feb.js"
+  },
+  {
+    "revision": "55bda3ce8f5b871496b7",
+    "url": "/music-vue/js/mall-category.b67cf858.js"
+  },
+  {
+    "revision": "d333620fa006e17b33bb",
+    "url": "/music-vue/js/mall.da34e5c6.js"
+  },
+  {
+    "revision": "ca2f5864abc45c0cdcaf",
+    "url": "/music-vue/js/playlist-detail.8a9ebe43.js"
+  },
+  {
+    "revision": "b1c4214d39721d693084",
+    "url": "/music-vue/js/playlist.e147eba6.js"
+  },
+  {
+    "revision": "4483e560d118e56f748f",
+    "url": "/music-vue/js/rank.59901ff3.js"
+  },
+  {
+    "revision": "b0958f7570585bb6831f",
+    "url": "/music-vue/js/recommend.ee62135c.js"
+  },
+  {
+    "revision": "2bc386d717b4293f10e2",
+    "url": "/music-vue/js/search.7c03422e.js"
+  },
+  {
+    "revision": "46ee12f6cf6ab7a8ad35",
+    "url": "/music-vue/js/singer-detail.0773a7f9.js"
+  },
+  {
+    "revision": "ffc36ec116d61431ad11",
+    "url": "/music-vue/js/songs.1e8f90fe.js"
+  },
+  {
+    "revision": "178c223001efb16b9b6e",
+    "url": "/music-vue/js/user.920aeaee.js"
+  },
+  {
+    "revision": "63623da34d2a8405018ef49730e319aa",
+    "url": "/music-vue/mainifest.json"
+  },
+  {
+    "revision": "beb686ffdbec7cccdb7ff78bb1fbac33",
+    "url": "/music-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/music-vue/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
-importScripts("/music-vue/precache-manifest.aea074f6036d3017da493b66c133d144.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/music-vue/precache-manifest.32a8dab34a928ee154f24b48ff298e2b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
+/*eslint-disable*/
 workbox.core.setCacheNameDetails({
   prefix: 'music',
-  suffix: 'v1.0.0'
+  suffix: 'v1.0.0',
 });
 // 让我们的service worker尽快的得到更新和获取页面的控制权
-workbox.core.skipWaiting(); 
+workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
 /*
@@ -31,24 +32,23 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 workbox.routing.registerRoute(
   /^https:\/\/p1\.music\.126\.net/,
   workbox.strategies.networkFirst({
-  cacheName: "images",
+    cacheName: 'images',
     plugins: [
-     new workbox.expiration.Plugin({
-        maxAgeSeconds: 5 * 24 * 60 * 60 // 设置缓存有效期为5天
-      })
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 5 * 24 * 60 * 60, // 设置缓存有效期为5天
+      }),
     ],
   })
 );
 workbox.routing.registerRoute(
-  /^http:\/\/121\.4\.152\.254/,
+  /^https:\/\/netease-cloud-music-api-gilt\.vercel\.app/,
   workbox.strategies.networkFirst({
-  cacheName: "api",
+    cacheName: 'api',
     plugins: [
       new workbox.expiration.Plugin({
-        maxAgeSeconds: 24 * 60 * 60 // 设置缓存有效期为1天
-      })
+        maxAgeSeconds: 24 * 60 * 60, // 设置缓存有效期为1天
+      }),
     ],
   })
 );
-
 
